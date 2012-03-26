328bd41f75bfb95a2d68f2fb9603d173bd165afa
eddiemundo <document_done@hotmail.com>
2012-03-25 22:56:53 -0400

    First commit.

--- LoLReplaySite/lolreplaysite/views.py
+++ LoLReplaySite/lolreplaysite/views.py
@@ -1,5 +1,16 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.renderers import get_renderer
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    return {'project':'LoLReplaySite'}
+def site_layout():
+	renderer = get_renderer("templates/main.pt")
+	layout = renderer.implementation().macros['layout']
+	return layout
+
+@view_config(route_name='home', renderer='templates/main.pt')
+def home(request):
+    return {'layout': site_layout(), 'replay_list': []}
+
+@view_config(route_name='replays', renderer='templates/replays.pt')
+def replays(request):
+ 	return {'layout': site_layout(), 'replay_list': []}
